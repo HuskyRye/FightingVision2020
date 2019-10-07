f996a83f99d6a6f160fdee47395dcc07b36d37a8
HuskyRye <1412167533@qq.com>
2019-10-07 16:15:52 +0800

    Update FightingVision2020.cpp

--- FightingVision2020.cpp
+++ FightingVision2020.cpp
@@ -16,7 +16,11 @@ int main(int argc, char* argv[])
     }
 
     // 串口初始化
+#ifdef Windows
     SerialPort serial_port("COM1");
+#elif defined Linux
+    SerialPort serial_port("/dev/ttyUSB0");
+#endif
     if (!serial_port.Init()) {
         printf("Serial_port initialization failed.\n");
         return 1;
