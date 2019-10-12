621dd70d21c38be146feeed7b0817c001bf29900
HuskyRye <1412167533@qq.com>
2019-10-13 00:20:46 +0800

    Update Copyright

--- Armor/ArmorDetector.h
+++ Armor/ArmorDetector.h
@@ -1,3 +1,19 @@
+﻿/*
+ *  Copyright (C) 2019 刘臣轩
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
@@ -15,7 +31,7 @@ public:
 public:
     cv::RotatedRect rect;
     float stddev;
-    // TODO: classifier���ֵ
+    // TODO: classifier输出值
 };
 
 class ArmorDetector {
