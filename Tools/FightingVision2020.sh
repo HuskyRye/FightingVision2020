d01de5d3887c9da9b8f670393a5307dfa3f1693c
HuskyRye <1412167533@qq.com>
2019-10-15 21:02:10 +0800

    Update watchdog

--- Tools/FightingVision2020.sh
+++ Tools/FightingVision2020.sh
@@ -3,7 +3,7 @@
 #<the exe file's absolute path and its shell parameter>"
 # And then when ever the exe shutdown it will be automatically restart
 
-exe=$1
+exe=/home/dji/FightingVision2020/build/FightingVision2020
 while true; do
 	state=`ps aux | grep "$exe" | grep -v grep | grep -v $0`
 	if [ ! "$state" ]; then
